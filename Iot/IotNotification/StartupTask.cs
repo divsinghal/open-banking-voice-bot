84cdc84aa2a4e0f70bbb394870f3cb6b4bda3ad0
Divya Singhal <div.singhal@gmail.com>
2018-03-17 20:51:14 +0000

    Iot

---
+++ Iot/IotNotification/StartupTask.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using Windows.ApplicationModel.Background;
+using Windows.Devices.Gpio;
+using System.Threading.Tasks;
+
+// The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
+
+namespace IotNotification
+{
+    public sealed class StartupTask : IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            int led_Pin = 35;
+            var gpio = GpioController.GetDefault();
+
+            //var a = FindMessage();
+            GpioPin pin = gpio.OpenPin(led_Pin);
+            pin.SetDriveMode(GpioPinDriveMode.Output);
+            pin.Write(GpioPinValue.Low);
+            Task.Delay(1000).Wait();
+            pin.Write(GpioPinValue.High);
+            Task.Delay(1000).Wait();
+        }
+
+        //private object FindMessage()
+        //{
+        //    CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+        //        CloudConfigurationManager.GetSetting("StorageConnectionString"));
+        //    CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+        //}
+    }
+}
