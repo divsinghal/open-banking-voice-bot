a90be2e3afaff2f8e732f527507ded32603fca30
Ben Clayton <benjamin.clayton@brewin.co.uk>
2018-03-18 09:51:04 +0000

    - iot queue

--- Api/Azure4Alexa/Controllers/IotController.cs
+++ Api/Azure4Alexa/Controllers/IotController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
+﻿using System.Web.Http;
 using System.Threading.Tasks;
 
 namespace Azure4Alexa.Controllers
@@ -11,9 +6,10 @@ namespace Azure4Alexa.Controllers
     public class IotController : ApiController
     {
         [HttpGet]
-        public Task<Models.Accounts.Account> Get()
+        [Route("api/iot")]
+        public async Task<bool> Get()
         {
-            var response =  Azure4Alexa.Santander.GetBalance.GetBalanceFromOpenBanking();
+            var response = await Iot.GetPayments.HasPayment();
             return response;
         }
     }
---
+++ Api/Azure4Alexa/Iot/GetPayments.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+
+namespace Azure4Alexa.Iot
+{
+    public class GetPayments
+    {
+        public static async Task<bool> HasPayment()
+        {
+            var storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("StorageConnectionString"));
+            var queueClient = storageAccount.CreateCloudQueueClient();
+            var queue = queueClient.GetQueueReference("paymentqueue");
+            var retrievedMessage = await queue.GetMessageAsync();
+
+            if (retrievedMessage != null)
+            {
+                await queue.DeleteMessageAsync(retrievedMessage);
+                return true;
+            }
+            return false;
+        }
+    }
+}
